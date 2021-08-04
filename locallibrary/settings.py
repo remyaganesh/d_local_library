c02676bb15a8dd04669cb748e3a0009538d950ae
remyaganesh <remya.jun30@gmail.com>
2021-08-04 10:53:27 +0400

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 #export DJANGO_DEBUG=False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['morning-lowlands-27305.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
